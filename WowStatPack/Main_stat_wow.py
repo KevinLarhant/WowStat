721c415f9e8dcc0bbb05e15677f069d60d760c13
KevinLarhant <lkevin44@gmail.com>
2020-08-23 17:55:04 +0200

    marche plus

--- WowStatPack/Main_stat_wow.py
+++ WowStatPack/Main_stat_wow.py
@@ -5,9 +5,9 @@ APIPath = 'https://eu.api.blizzard.com'
 tokenURL = 'https://us.battle.net/oauth/token?grant_type=client_credentials'
 myOAuth = '2e7605b670ff412fb379e5ea706bca2f'
 mySecret = '37oFLU6xjDwZ8DY6HDVSavGfhIUVJM0F'
-credentials = '&client_id={0}&client_secret={1}'.format(myOAuth, mySecret)
+credentials = f'&client_id={myOAuth}&client_secret={mySecret}'
 paramz = '?locale=en_US&access_token='
-#realm
+# realm
 dal = 'dalaran'
 ys = 'Ysondre'
 
@@ -27,7 +27,7 @@ def get_basic_info_char_by_server_name(realm, char):
 
 def get_all_stat_by_server_name(realm, char, token):
     return request_to_api(
-        '/wow/character/{0}/{1}?fields=statistics&locale=en_US&access_token={2}'.format(realm, char, token))
+        f'/wow/character/{realm}/{char}?fields=statistics&locale=en_US&access_token={token}&namespace=static-eu')
 
 
 def get_bg_done(statJson):
@@ -41,7 +41,7 @@ def get_bg_won(statJson):
 
 
 def get_gold_CM_done(statJson):
-    return statJson['statistics']['subCategories'][14]['statistics'][12]
+    res = statJson['statistics']['subCategories'][14]['statistics'][12]
     return res['quantity']
 
 
@@ -55,33 +55,39 @@ def write_stat(realm, char, token):
 def add_my_char():
     c = {'aktø': dal, 'waktorr': dal, 'kishaa': dal, 'klehia': dal, 'aktto': dal, 'kamss': dal, 'kaezia': dal,
          'kboom': dal, 'wakito': dal, 'keanna': dal, 'kylx': dal, 'kyootie': dal, 'aktok': dal, 'akkto': dal,
-         'Aktø': ys, 'kziin': ys, 'akto': ys}
+         'Aktø': ys, 'kziin': ys, 'akto': ys}  # todo :get from config file
     return c
 
 
+def create_json_file():
+    my_char = add_my_char()
+    for char in my_char:
+        write_stat(my_char[char], char, token)
+
+
+def main():
+    # call_1_char = get_char_by_server_name('Dalaran', 'Waktorr')
+    # print('i call :' +APIPath + call_1_char + paramz + token)
+    # info = requests.get(APIPath + call_1_char + paramz + token)
+    # print(info.json())
+
+    # charge fichier json stat war
+    # file = "/jsonStats/dalaran_waktorr_stat.json"
+    # with open(file, "r") as f:
+    #     statWar = json.load(f)
+
+    # recup / creation fichier stat all persos - UTILE 1 FOIS
+    create_json_file()
+
+    # test et print qq stat
+    # print(get_bg_done(statWar))
+    # print(get_bg_won(statWar))
+    # print(get_gold_CM_done(statWar))
+
+    # stat url :
+    # https://eu.api.blizzard.com/wow/character/dalaran/waktorr?fields=statistics&locale=en_US&access_token=USRYSRgBCOpB1XHItm8dI8qFAX6NDcs8BI
+
+
 token = get_token(tokenURL + credentials)
-
-# call_1_char = get_char_by_server_name('Dalaran', 'Waktorr')
-# print('i call :' +APIPath + call_1_char + paramz + token)
-# info = requests.get(APIPath + call_1_char + paramz + token)
-# print(info.json())
-
-# charge  fichier json stat war
-# file = "/jsonStats/dalaran_waktorr_stat.json"
-# with open(file, "r") as f:
-#     statWar = json.load(f)
-
-
-#recup / creation fichier stat all persos
-my_char = add_my_char()
-for char in my_char:
-    write_stat(my_char[char], char, token)
-
-
-# test et print qq stat
-# print(get_bg_done(statWar))
-# print(get_bg_won(statWar))
-# print(get_gold_CM_done(statWar))
-
-# stat url :
-# https://eu.api.blizzard.com/wow/character/dalaran/waktorr?fields=statistics&locale=en_US&access_token=USRYSRgBCOpB1XHItm8dI8qFAX6NDcs8BI
+print('token is : ', token)
+main()
